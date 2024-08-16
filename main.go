eaa3cc6b152662ba8645773015422a4f1c1c4b0d
김예환 Ye-Hwan Kim (Sam) <yehwankim23@gmail.com>
2024-08-16 21:24:25 +0900

    Add Firebase

--- main.go
+++ main.go
@@ -1,11 +1,34 @@
 package main
 
 import (
+	"context"
 	"encoding/json"
 	"log"
 	"net/http"
 	"time"
+
+	firebase "firebase.google.com/go"
+	"google.golang.org/api/option"
 )
+
+var CONTEXT context.Context
+var FIREBASE_APP *firebase.App
+
+func initializeFirebase() {
+	var err error
+
+	FIREBASE_APP, err = firebase.NewApp(
+		CONTEXT,
+		nil,
+		option.WithCredentialsFile("click-utah-d7eaac9e6640.json"),
+	)
+
+	if err != nil {
+		log.Fatalln(err)
+	}
+
+	log.Println("Initialized Firebase")
+}
 
 func handlePing(responseWriter http.ResponseWriter, request *http.Request) {
 	location, err := time.LoadLocation("Asia/Seoul")
@@ -34,6 +57,9 @@ func handlePing(responseWriter http.ResponseWriter, request *http.Request) {
 }
 
 func main() {
+	CONTEXT = context.Background()
+	initializeFirebase()
+
 	http.HandleFunc("/ping", handlePing)
 
 	log.Fatalln(http.ListenAndServe(":80", nil))
